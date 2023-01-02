55e8d3c13d71ef5e320c091278df2b8edf3c3211
Grace Richardson <graceannx@icloud.com>
2023-01-02 14:15:01 +0000

    Test scenarios, debugging edge case logic

--- src/player.py
+++ src/player.py
@@ -6,8 +6,9 @@ class Player(Deck):
         self.hand = []
         self.name = ""
         self.score = 0
-        self.Aceflag = False
+        self.aceflag = False
         self.ingame = True
+        self.bustflag = False
         self.hit(2, Deck)
 
     def __str__(self):
@@ -18,26 +19,38 @@ class Player(Deck):
             for i in range(number_of_cards):
                     popcard = Deck.cards.pop()
                     if popcard.rank == "Ace":
-                        Aceflag = True 
+                        self.aceflag = True 
                         if self.score > 10:
                             popcard.score = 1
-                    self.score += popcard.score
                     self.hand.append(popcard)
-                    if self.validate_hand() == False:
+                    if self.add_score(popcard.score) == False:
                         print("You have busted!")
+                        self.bustflag = True
                         self.ingame = False
                         break
         else: 
             print("You are out of the game!")
 
             
-    
+    def stand(self, Deck):
+        self.stand_score = self.score
+        self.ingame = False
+        print("You have chosen to stand!")
+        print("Your score is: ", self.stand_score)
+        print("Your hand is: ")
+        for i in range(len(self.hand)):
+            print(self.hand[i].rank)
+            print(self.hand[i].suit)
 
     def discard(self):
         return self.hand.pop()
 
     def add_score(self, score):
         self.score += score
+        if self.aceflag == True and self.score > 21:
+            self.ace_adjust()
+        return self.validate_hand()
+        
 
     def reset_score(self):
         self.score = 0
@@ -49,8 +62,27 @@ class Player(Deck):
         self.name = input("What is your name? ")
 
     def validate_hand(self):
+       # if self.score != self.check_score():
+      #      print("ERROR: Score is not equal qto the sum of the cards in the hand!")
+     #       return False
+
         if self.score > 21:
+            
+            self.ace_adjust()
             return False
         else:
             return True
+    
+    def check_score(self):
+        checkscore = 0
+        for checkcard in self.hand: 
+            checkscore += checkcard.score
+        return checkscore
+    
+    def ace_adjust(self):
+        if self.score > 10:
+            for i in range(len(self.hand)):
+                if self.hand[i].rank == "Ace" and self.hand[i].score == 11 and self.score > 21:
+                    self.hand[i].score = 1
+                    self.score -= 10
         
