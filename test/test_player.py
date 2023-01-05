759d8c5be6f2a1a4521d4b04caf4706f6c0b6715
Grace Richardson <graceannx@icloud.com>
2023-01-05 11:42:44 +0000

    Final commit for the Play Method

--- test/test_player.py
+++ test/test_player.py
@@ -46,7 +46,7 @@ class PlayerTestCase(unittest.TestCase):
 
 
     def test_scenario_5(self): #Given my score is updated, When it is 22 or more, Then I am ‘bust’ and do not have a valid hand
-        self.player.hit(5, self.deck)
+        self.player.hit(22, self.deck) #if there are 22 cards in the hand, there is no way to get a score of less than 22
         self.assertEqual(self.player.bustflag, True)
 
     def test_scenario_6(self): #Given I have a king and an ace,When my score is evaluated, Then my score is 21
@@ -59,19 +59,22 @@ class PlayerTestCase(unittest.TestCase):
 #        print(score1)
 
     def test_scenario_7(self): #Given I have a king, a queen, and an ace, When my score is evaluated, Then my score is 21
+        #In the play method, this validation is triggered by the hit method, but here I want to use a custom hand. 
+        # To bypass this, I create a hand and then pass it to the add_score method, where the validation occurs within the hit method.
         self.player.reset_hand()
         self.player.reset_score()
         self.player.hand.append(Card("Spades", "King"))
         self.player.hand.append(Card("Spades", "Queen"))
         self.player.hand.append(Card("Spades", "Ace"))
-        self.player.acecount = 1
-        self.player.add_score(self.player.check_score()) #here, check_score is used to sum the hand created through appending, and return the score of the hand. This score is passed into add_score which adjusts the scores of the aces in the hand (from 11 to 1) if necessary.
+        self.player.acecount = 1 #the aces are counted in my hit method, so this is hardcoded here to bypass for testing.
+        self.player.add_score(self.player.check_score()) #here, check_score is used to sum the hand custom hand, and return its score. The score and, by proxy, hand is passed into add_score which adjusts the scores of the aces in the hand (from 11 to 1) if necessary.
         score2 = self.player.check_score() #check_score is used again to sum the new hand, and return the score of the hand.
 #        self.player.show_hand()
         self.assertEqual(score2, 21) 
  #       print(score2)
     
     def test_scenario_8(self): #Given I have a king, a queen, and an ace, When my score is evaluated, Then my score is 21
+        #the same procedure as scenario 7 is used to test this custom hand.
         self.player.reset_hand()
         self.player.reset_score()
         self.player.hand.append(Card("Spades", "Ace"))
@@ -87,6 +90,7 @@ class PlayerTestCase(unittest.TestCase):
  #       print(score3)
 
     def test_scenario_9(self): # Given that I have a nine, an ace, and another ace,When my score is evaluated, Then my score is 21
+        #the same procedure as scenario 7 is used to test this custom hand.
         self.player.reset_hand()
         self.player.reset_score()
         self.player.hand.append(Card("Spades", "9"))
